2d51f8e32d91a6aa518e002755f3858738f30042
Zafer35-sos <zaferyildirim862010@gmail.com>
2025-08-20 10:02:57 +0300

    ai_response.py

--- ai_response.py
+++ ai_response.py
@@ -10,7 +10,8 @@ def chat_with_gmail_agent(email_text):
             },
         ]
     )
-    return response['message']['content']  # Adjust if response format is different
+    return response['message']['content'] 
 
 def summarize_email(email_text):
     return chat_with_gmail_agent(email_text)
+
